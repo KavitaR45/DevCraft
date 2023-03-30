15de0f2e6358f3ae2e3b39b32b6a0af238866e80
@Kavita2599 <kavita99@netart.io>
2023-03-30 18:23:39 +0530

    icon added

--- components/seo.js
+++ components/seo.js
@@ -4,6 +4,7 @@ const SEO = ({ title, description, canonicalUrl }) => {
   return (
     <Head>
       <title>{title}</title>
+      <link rel='icon' href='/images/logo.jpg'/>
       <meta name="description" content={description} />
       <link rel="canonical" href={canonicalUrl} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
