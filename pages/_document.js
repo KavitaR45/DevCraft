774065aa7851a1e1ca9cf395f4208314ed64193c
@Kavita2599 <kavita99@netart.io>
2023-04-04 11:48:58 +0530

    tracking code added

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,7 @@ export default function Document() {
               `,
             }}
           />
-          {/* <script
+          <script
             type="text/javascript"
             dangerouslySetInnerHTML={{
               __html: `
@@ -26,7 +26,7 @@ export default function Document() {
                     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "g9dfynwutm");
+                })(window, document, "clarity", "script", "gj54bm0k54");
               `,
             }}
           />
@@ -35,7 +35,7 @@ export default function Document() {
               __html: `
           (function(h,o,t,j,a,r){
               h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:3409788,hjsv:6};
+              h._hjSettings={hjid:3436673,hjsv:6};
               a=o.getElementsByTagName('head')[0];
               r=o.createElement('script');r.async=1;
               r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
@@ -43,7 +43,7 @@ export default function Document() {
           })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
         `,
             }}
-          /> */}
+          />
       </Head>
       <body>
         <Main />
